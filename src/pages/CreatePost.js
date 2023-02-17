dda03563381fb0609a1effcd9a5507fd59f4e7a0
danielyyi <daniel.youngsun@gmail.com>
2023-02-17 18:36:29 -0500

    init

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -32,10 +32,42 @@ function MakePost(props) {
     setColor(color);
   }
 
+  function resizeImage(base64Str, maxWidth = 200, maxHeight = 200) {
+    return new Promise((resolve) => {
+      let img = new Image()
+      img.src = base64Str
+      img.onload = () => {
+        let canvas = document.createElement('canvas')
+        const MAX_WIDTH = maxWidth
+        const MAX_HEIGHT = maxHeight
+        let width = img.width
+        let height = img.height
+  
+        if (width > height) {
+          if (width > MAX_WIDTH) {
+            height *= MAX_WIDTH / width
+            width = MAX_WIDTH
+          }
+        } else {
+          if (height > MAX_HEIGHT) {
+            width *= MAX_HEIGHT / height
+            height = MAX_HEIGHT
+          }
+        }
+        canvas.width = width
+        canvas.height = height
+        let ctx = canvas.getContext('2d')
+        ctx.drawImage(img, 0, 0, width, height)
+        resolve(canvas.toDataURL())
+      }
+    })
+  }
+
   const [image, setImage] = useState('');
   function changeImage(image){
     values.image = image;
-    setImage(image);
+
+    resizeImage(image, 200, 200).then((result) => setImage(result));
   }
 
   const [createPost, { error }] = useMutation(CREATE_POST_MUTATION, {
