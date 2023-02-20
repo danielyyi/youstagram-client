ceab03a847d4e9e8b75812278bf0b6e79f76a484
danielyyi <daniel.youngsun@gmail.com>
2023-02-19 22:30:27 -0500

    init

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -6,8 +6,8 @@ import {setContext} from 'apollo-link-context';
 
 
 const httpLink = createHttpLink({
-    //uri: 'https://youstagram-backend.onrender.com'
-    uri: 'http://localhost:5000/graphql'
+    uri: 'https://youstagram-backend.onrender.com'
+    //uri: 'http://localhost:5000/graphql'
 })
 
 const authLink = setContext(() => {
